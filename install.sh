cd1e4fa264f16afed72e60f745b76a4080bc2ab0
nisanharamati <3074526+nisanharamati@users.noreply.github.com>
2026-02-26 09:42:37 -0800

    add install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+ln -s `pwd`/ag_quotatop ~/.local/bin/ag_top
+echo to '~/.local/bin/ag_top'
+echo run with 'ag_top' to print and exit, or 'ag_top -l [60 seconds]' for a top-like display with user provided refresh rate \(default 60 seconds\)
